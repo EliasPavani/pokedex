9a02905572edcf26313136532f24288fe2a78bb2
Elias Pavani <elias.gf.pavani@gmail.com>
2023-06-09 00:53:22 -0300

    Atualização do modal com todos status do pokemon
    Co-authored-by: Caio Amaral <caio.amaral.lds@gmail.com>

--- src/assets/js/data.js
+++ src/assets/js/data.js
@@ -5,23 +5,19 @@ function exibirPokemons(pokemons) {
     
     pokemons.forEach(function (pokemon) {
         var li = document.createElement("li")
-
         li.classList.add("card-pokemon")
+        li.onclick = () => showPokemon(pokemon)
 
         var div = document.createElement("div")
-
         div.classList.add("informations")
 
         var spanName = document.createElement("span")
-
         spanName.innerText = pokemon.name.english
 
         var spanId = document.createElement("span")
-
         spanId.innerText = "#" + pokemon.id.toString().padStart(3, "0")
 
         var img = document.createElement("img")
-
         var src = pokemon.image.thumbnail
 
         if (!src) {
@@ -37,7 +33,6 @@ function exibirPokemons(pokemons) {
         img.classList.add("gif")
 
         var ul = document.createElement("ul")
-
         ul.classList.add("types")
 
         var types = []
@@ -53,12 +48,6 @@ function exibirPokemons(pokemons) {
             types.push(li)
         })
 
-        var p = document.createElement("p")
-
-        p.classList.add("description")
-
-        p.innerText = pokemon.description
-
         div.appendChild(spanName)
         div.appendChild(spanId)
 
@@ -69,10 +58,58 @@ function exibirPokemons(pokemons) {
         li.appendChild(div)
         li.appendChild(img)
         li.appendChild(ul)
-        li.appendChild(p)
 
         listNode.appendChild(li)
     })
+}
+
+function showPokemon(data) {
+    var overlay = document.getElementById("overlay")
+    var container = document.getElementById("details")
+
+    var nameContainer = container.getElementsByClassName("name")[0]
+    var numberContainer = container.getElementsByClassName("number")[0]
+    var statsContainer = container.getElementsByClassName("stats")[0]
+    var typesContainer = container.getElementsByClassName("types")[0]
+    var descriptionActiveContainer = container.getElementsByClassName("descriptionActive")[0]
+    var img = container.getElementsByClassName("image-active")[0]
+
+    nameContainer.innerText = data.name.english
+    numberContainer.innerText = "#" + data.id.toString().padStart(3, "0")
+    descriptionActiveContainer.innerText = data.description
+
+    Array.from(typesContainer.children).forEach(node => node.remove())
+
+    data.type.forEach(function (type) {
+        var div = document.createElement("div")
+
+        div.classList.add("type")
+        div.classList.add(type.toLowerCase())
+
+        div.innerText = type
+
+        typesContainer.appendChild(div)
+    })
+
+    img.src = data.image.thumbnail
+
+    statsContainer.getElementsByClassName("hp")[0].innerText = data.base["HP"]
+    statsContainer.getElementsByClassName("attack")[0].innerText = data.base["Attack"]
+    statsContainer.getElementsByClassName("defense")[0].innerText = data.base["Defense"]
+    statsContainer.getElementsByClassName("sp-attack")[0].innerText = data.base["Sp. Attack"]
+    statsContainer.getElementsByClassName("sp-defense")[0].innerText = data.base["Sp. Defense"]
+    statsContainer.getElementsByClassName("speed")[0].innerText = data.base["Speed"]
+
+    container.classList.add("active")
+    overlay.classList.add("active")
+}
+
+function closeDetails () {
+    var container = document.getElementById("details")
+    container.classList.remove("active")
+
+    var overlay = document.getElementById("overlay")
+    overlay.classList.remove("active")
 }
 
 var listaPokemons = []
